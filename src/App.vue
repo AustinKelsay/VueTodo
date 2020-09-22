4fdbb2f1053bffa15380eab85358bb21a9ddc1e6
austinkelsay <austinkelsay@yahoo.com>
2020-09-22 17:47:51 -0500

    Got router implemented and it's at MVP!

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,40 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view/>
   </div>
 </template>
 
 <script>
 import Header from "./components/layout/Header"
-import Todos from "./components/Todos"
-import AddTodo from "./components/addTodo"
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    Todos,
-    Header,
-    AddTodo
-  },
-  // where we will store todo's data
-  // data is a function that returns objects to the ui
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo one",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo two",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "Todo three",
-          completed: false
-        }
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
-    }
+    Header
   }
 }
-</script>
-
+</script>'
+'
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
+#nav {
+  padding: 30px;
 }
 
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
-  color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
-.btn:hover {
-  background: #666;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
